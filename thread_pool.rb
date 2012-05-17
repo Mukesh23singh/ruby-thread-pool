1ac198c69f59d77caa541425354b760c3d7acfae
Jürgen Bickert <juergenbickert@gmail.com>
2012-05-18 01:27:37 +0200

    finished thread pool

---
+++ thread_pool.rb
@@ -0,0 +1,56 @@
+require "thread"
+
+class ThreadPool
+  def initialize(pool_size)
+    @pool = []
+    @pool_size = pool_size
+    @pool_mutex = Mutex.new
+    @pool_cv = ConditionVariable.new
+  end
+
+  def run(*args)
+    Thread.new do
+      @pool_mutex.synchronize do
+        while @pool.size >= @pool_size
+          print "Pool is full: waiting to run #{args.join(",")}\n" if $DEBUG
+          @pool_cv.wait(@pool_mutex)
+        end
+      end
+      @pool << Thread.current
+      begin
+        yield(*args)
+      rescue => e
+        on_exception(self, e, *args)
+      ensure
+        @pool_mutex.synchronize do
+          @pool.delete(Thread.current)
+          @pool_cv.signal
+        end
+      end
+    end
+  end
+
+  alias_method :new, :run
+
+  def join
+    sleep 0.001 # needed because join must be called after threads
+                # had chance to lock
+    @pool_mutex.synchronize { @pool_cv.wait(@pool_mutex) until @pool.empty? }
+  end
+
+  def on_exception(thread, exception, *original_args)
+    # if you want to handle exception subclass and implement this method
+  end
+end
+
+class Array
+  def peach(size=nil, type=ThreadPool, &block)
+    if size == nil
+      each { |*args| Thread.new(*args, &block) }
+    else
+      pool = type.new(size)
+      each { |*args| pool.new(*args, &block) }
+      pool.join
+    end
+  end
+end
---
+++ thread_pool_test.rb
@@ -0,0 +1,26 @@
+require "minitest/autorun"
+require_relative "thread_pool"
+
+describe "ThreadPool" do
+  it "should run at max so many threads" do
+    require "peach"
+    start = Time.now
+    20.times.to_a.peach(10) { sleep(0.1) }
+    stop = Time.now
+    time = stop - start
+    time.must_be :>=, 0.2
+    time.must_be :<=, 0.3
+  end
+
+  it "should not stop on long running threads" do
+    a = [0.01,0.01,0.2,0.2, 0.01, 0.01,0.01,0.01,0.01,0.01]
+    start = Time.now
+    a.peach(5) do |time|
+      sleep time
+    end
+    stop = Time.now
+    elapsed = stop - start
+    elapsed.must_be :>=, 0.2
+    elapsed.must_be :<=, 0.3
+  end
+end
